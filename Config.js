63c0193df0cd1839b5790c3296a64b5f2b2d133f
Takeoffhacker <166225930+Takeoffhacker@users.noreply.github.com>
2024-04-06 13:36:19 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['088429245']
+global.ownername = "Takeoff"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "sierra Leonean"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'Takeoff'
 global.packname = 'Sticker By'
 global.author = 'Maria Bot'
 //console view/theme
